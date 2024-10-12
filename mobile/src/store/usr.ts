f6d78f2d378117e810da8aa4c569e1b7b2f48dff
xiahao <47087639@qq.com>
2024-10-12 10:48:49 +0800

    fix: 修复退出登录状态不更新问题

--- mobile/src/store/usr.ts
+++ mobile/src/store/usr.ts
@@ -1,24 +1,27 @@
 import { StoreOptions } from "vuex";
 import { ESex, IUserInfo } from "@/api/usr/const";
 import { checkLoginStatusReq } from "@/api/usr";
+import { cloneDeep } from "lodash";
 
 export interface IUserState {usrInfo: IUserInfo}
 
+export const DEFAULT_USR_INFO = {
+  uid: "",
+  username: "",
+  avatar: "",
+  nick: "",
+  mail: "",
+  hasChangeNick: false,
+  sex: ESex.unknow,
+  lastLoginTime: 0,
+  followNum: 0,
+  likesNum: 0,
+  collectNum: 0,
+}
+
 export default {
   state: {
-    usrInfo: {
-      uid: "",
-      username: "",
-      avatar: "",
-      nick: "",
-      mail: "",
-      hasChangeNick: false,
-      sex: ESex.unknow,
-      lastLoginTime: 0,
-      followNum: 0,
-      likesNum: 0,
-      collectNum: 0,
-    }
+    usrInfo: cloneDeep(DEFAULT_USR_INFO)
   },
   mutations: {
     updateAllUserInfo: (state, payload: IUserInfo) => {
@@ -29,6 +32,7 @@ export default {
     checkLoginStatus: async (context) => {
       const res = await checkLoginStatusReq();
       if (res.code === 200) context.commit("updateAllUserInfo", res.data);
+      else context.commit("updateAllUserInfo", cloneDeep(DEFAULT_USR_INFO));
     },
   },
   modules: {},
